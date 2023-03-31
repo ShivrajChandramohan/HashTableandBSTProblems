bf3da2e8db8fec37d38ffe30cb234e18cd883a14
Shivrajchandramohan <shivrajchandramohan@gmail.com>
2023-04-01 02:24:56 +0530

    Insert the value

--- Program.cs
+++ Program.cs
@@ -1,10 +1,17 @@
-﻿namespace BinarySearchTreeAssignment
+﻿using BinarySearchTreeAssigment;
+
+namespace BinarySearchTreeAssignment
 {
     class Program
     {
         public static void Main(string[] args)
         {
             Console.WriteLine("*****Welcome to the Hash Table and Bst problem*****");
+            BinarySearchTree<int> binarySearch = new BinarySearchTree<int>(56);
+            binarySearch.Add(30);
+            binarySearch.Add(70);
+            binarySearch.Display();
+
         }
     }
 }
